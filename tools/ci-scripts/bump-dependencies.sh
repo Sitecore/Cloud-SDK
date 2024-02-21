9e7aa81e4f4203f403c72178b92e9cce52a8c14e
Nikolaos Lazaridis (Νικόλαος Λαζαρίδης) <nikolaos.lazaridis@sitecore.com>
2024-02-21 12:51:58 +0200

    CDK-165 Create release pipeline step with script to update dependencies before release (#124)
    * ci: added new step and script to bump dependencies

---
+++ tools/ci-scripts/bump-dependencies.sh
@@ -0,0 +1,18 @@
+readarray -t affected_packages < <(npx nx show projects --affected --base="$1" --exclude="apps/*,@sitecore-cloudsdk")
+cd packages
+for affected in "${affected_packages[@]}"; do
+    new_version="^$(cd $affected && npm pkg get version | jq -r '.[]' && cd ..)"
+    echo "Running checks for $affected package with new version:$new_version"
+    for package in "${affected_packages[@]}"; do
+        if [ "$affected" = "$package" ]; then
+            continue
+        fi
+        cd $package
+        prop="dependencies.@sitecore-cloudsdk/$affected"
+        if [ $(npm pkg get $prop | jq -r '.[]') != "{}" ]; then
+            npm pkg set $prop=$new_version >/dev/null
+            echo "Updated @sitecore-cloudsdk/$affected in @sitecore-cloudsdk/$package to $new_version version."
+        fi
+        cd ..
+    done
+done
